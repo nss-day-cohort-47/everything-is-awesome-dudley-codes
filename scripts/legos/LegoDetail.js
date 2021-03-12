501345f27fa6e4fd5fc0172b296a82ce32d9b2be
Alex Dudley <alexdudley@me.com>
2021-03-12 10:55:23 -0600

    added function that adds notes toLego card

--- scripts/legos/LegoDetail.js
+++ scripts/legos/LegoDetail.js
@@ -1,20 +1,29 @@
 import { invertColor } from './../helpers.js';
 
 export const LegoDetail = (brick) => {
+  const hasNotes = () => {
+    const legoNotes = brick.Notes;
+    if (legoNotes) {
+    return legoNotes
+    } else {
+    return "";
+}}
 
   let block = `<section class="block-wrapper" style="background-color:#${brick.ColorHex}">
               <h3>Name: ${brick.LegoName}</h3>
               <div class="block-years">Manufactured ${brick.YearFrom} - ${brick.YearTo}</div>
+              <div class="block-notes" >Notes: ${hasNotes()}</div>
+              </section>
               `;
+
   const link = brick.ColorstreamLinkImage;
-  if (link) {
-    //true? wrap the block in an a tag
-    return `
-      <a href="${link}" target="_blank" style="color:#${invertColor(brick.ColorHex)}">
-				${block}
-			</a>`;
-  } else {
-    //false? return the block
-    return block;
-  }
-}
+    if (link) {
+      //true? wrap the block in an a tag
+      return `
+        <a href="${link}" target="_blank" style="color:#${invertColor(brick.ColorHex)}">
+          ${block}
+        </a>`;
+    } else {
+      //false? return the block
+      return block;
+}}
